e5ebf631e7245247bb24c68f404575c91ae442ba
Fabrício Henrique <fabricio.henrique@gft.com>
2019-05-27 21:27:42 -0300

    update

---
+++ app/src/main/java/br/com/fabriciohsilva/cooltrips/view/details/PackageDetailsActivity.java
@@ -0,0 +1,15 @@
+package br.com.fabriciohsilva.cooltrips.view.details;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.fabriciohsilva.cooltrips.R;
+
+public class PackageDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_package_details);
+    }
+}
