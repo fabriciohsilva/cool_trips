e69ee155043c06ca2bc860aacf455aedd1d1c4d5
Fabrício Henrique <fabricio.henrique@gft.com>
2019-05-28 14:49:32 -0300

    update

--- app/src/main/java/br/com/fabriciohsilva/cooltrips/util/DaysUtil.java
+++ app/src/main/java/br/com/fabriciohsilva/cooltrips/util/DaysUtil.java
@@ -2,10 +2,14 @@ package br.com.fabriciohsilva.cooltrips.util;
 
 import android.support.annotation.NonNull;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class DaysUtil {
 
     public static final String PLURAL = " dias";
     public static final String SINGULAR = " dia";
+    public static final String day_month = "dd/MM";
 
     @NonNull
     public static String formatInText(int daysQuantity) {
@@ -13,6 +17,19 @@ public class DaysUtil {
             return daysQuantity + PLURAL;
         }
         return daysQuantity + SINGULAR;
+    }
+
+    @NonNull
+    public static String periodInText(int days) {
+        Calendar goDate = Calendar.getInstance();
+        Calendar backDate = Calendar.getInstance();
+        backDate.add(Calendar.DATE, days);
+
+        SimpleDateFormat brasilDateFormat = new SimpleDateFormat(day_month);
+        String goDateFormated = brasilDateFormat.format(goDate.getTime());
+        String backDateFormated = brasilDateFormat.format(backDate.getTime());
+
+        return goDateFormated + " - " + backDateFormated + " de " + backDate.get(Calendar.YEAR);
     }
 
 }
--- app/src/main/java/br/com/fabriciohsilva/cooltrips/view/details/PackageDetailsActivity.java
+++ app/src/main/java/br/com/fabriciohsilva/cooltrips/view/details/PackageDetailsActivity.java
@@ -1,6 +1,7 @@
 package br.com.fabriciohsilva.cooltrips.view.details;
 
 import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import br.com.fabriciohsilva.cooltrips.R;
 import br.com.fabriciohsilva.cooltrips.model.Package;
@@ -17,30 +20,52 @@ import br.com.fabriciohsilva.cooltrips.util.ResourceUtil;
 
 public class PackageDetailsActivity extends AppCompatActivity {
 
+    public static final String APPBAR_TITLE = "Resumo do Pacote";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_package_details);
 
+        setTitle(APPBAR_TITLE);
+
         Package pckgSP = new Package("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
 
-        TextView place = findViewById(R.id.package_details_place);
-        place.setText(pckgSP.getPlace());
+        showPlace(pckgSP);
+        showImage(pckgSP);
+        showDays(pckgSP);
+        showPrice(pckgSP);
+        showDate(pckgSP);
 
-        ImageView image = findViewById(R.id.package_details_image);
-        Drawable pckgDrawable = ResourceUtil.returnDrawable(this, pckgSP.getImage());
-        image.setImageDrawable(pckgDrawable);
+    }
 
-        TextView days = findViewById(R.id.package_details_days);
-        String daysInString = DaysUtil.formatInText(pckgSP.getDays());
-        days.setText(daysInString);
-
-        TextView price = findViewById(R.id.package_details_price);
-        String brasilianCurrency = CurrencyUtil.formatBrasilianString(pckgSP.getPrice());
-        price.setText(brasilianCurrency);
-
+    private void showDate(Package pckg) {
         TextView date = findViewById(R.id.package_details_date);
+        String pckgDateFormated = DaysUtil.periodInText(pckg.getDays());
+        date.setText(pckgDateFormated);
+    }
 
 
+    private void showPrice(Package pckg) {
+        TextView price = findViewById(R.id.package_details_price);
+        String brasilianCurrency = CurrencyUtil.formatBrasilianString(pckg.getPrice());
+        price.setText(brasilianCurrency);
+    }
+
+    private void showDays(Package pckg) {
+        TextView days = findViewById(R.id.package_details_days);
+        String daysInString = DaysUtil.formatInText(pckg.getDays());
+        days.setText(daysInString);
+    }
+
+    private void showImage(Package pckg) {
+        ImageView image = findViewById(R.id.package_details_image);
+        Drawable pckgDrawable = ResourceUtil.returnDrawable(this, pckg.getImage());
+        image.setImageDrawable(pckgDrawable);
+    }
+
+    private void showPlace(Package pckg) {
+        TextView place = findViewById(R.id.package_details_place);
+        place.setText(pckg.getPlace());
     }
 }
